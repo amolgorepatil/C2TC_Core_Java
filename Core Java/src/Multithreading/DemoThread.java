e4d1b1ffd04cec259851d8beb229eac2266df8c9
Patil-PC <Patil-PC@192.168.0.6>
2022-07-23 09:26:08 +0530

    commit

---
+++ Core Java/src/Multithreading/DemoThread.java
@@ -0,0 +1,32 @@
+// Create Thread Using Thread Class
+package Multithreading;
+
+class ChildThread extends Thread // Thread
+
+{
+	public void run()
+	{
+		for (int i=0;i<10;i++)
+		{
+			System.out.println("I am ChildThread");
+			Thread.yield();
+		}
+	}
+	
+}
+
+// Thread Scheduler
+public class DemoThread {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 ChildThread t=new ChildThread();
+		   t.start();
+		   
+		for (int i=0;i<10;i++)
+		{
+			System.out.println("I am MainThread");
+		}
+	}
+
+}
