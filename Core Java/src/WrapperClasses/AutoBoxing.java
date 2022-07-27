7e415fe8bf1c30a717739735e8316732385332dc
Patil-PC <Patil-PC@192.168.0.6>
2022-07-27 11:32:54 +0530

    commit

---
+++ Core Java/src/WrapperClasses/AutoBoxing.java
@@ -0,0 +1,15 @@
+package WrapperClasses;
+
+public class AutoBoxing {
+
+	public static void main(String[] args) {
+		
+		int a=20;
+		Integer i =a ;	//converting into integer
+		Integer j = new Integer(10);
+		
+		System.out.println(a+ " " + i+ " " +j);
+		
+	}
+
+}
---
+++ Core Java/src/WrapperClasses/Unboxing.java
@@ -0,0 +1,16 @@
+package WrapperClasses;
+
+public class Unboxing {
+
+	public static void main(String[] args) {
+		Character ch= 'a';	//	Unboxing - Character objects into primitive conversion
+		
+		char a = ch;
+		
+		System.out.println( "Character ch: " +ch);
+		
+		System.out.println("char a : " +a);
+		
+	}
+
+}
