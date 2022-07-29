5423d0b7d8376c0297dd4a678a5b64a71689a73d
Patil-PC <Patil-PC@192.168.0.6>
2022-07-30 00:58:19 +0530

    c

---
+++ Core Java/src/WrapperClasses/WrapperClassOne.java
@@ -0,0 +1,16 @@
+package WrapperClasses;
+
+public class WrapperClassOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a= 15;
+		Integer n1= Integer.valueOf(a);
+		Integer j = a;
+	
+		System.out.println("Int Value : "+a);
+		System.out.println("Integer Object: "+n1);
+		System.out.println(j);
+	}
+
+}
